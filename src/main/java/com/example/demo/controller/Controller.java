aa987490d7573b6fe3f9848b5cbb36494c302926
Edge Opsx <edgeops@ust-global.com>
2024-02-12 16:08:13 +0000

    VDCHDEPLOY-00: Published by PACE Conveyor

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -1,13 +1,13 @@
 package com.example.demo.controller;
 
 import com.example.demo.utils.CookieUtils;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 
 @CrossOrigin("*")
 @RestController
